ad667fa0cf9889dcf402f6a8a96a2f78320f624a
Connie Bernardin <conniebernardin@googlemail.com>
2022-02-08 18:16:09 +0000

    write test for checking most common occurence

--- src/test/java/DataStructureExercises/MostOccurancesTest.java
+++ src/test/java/DataStructureExercises/MostOccurancesTest.java
@@ -2,18 +2,27 @@ package DataStructureExercises;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MostOccurancesTest {
 
     @Test
-    void check() {
+    void itShouldCheckForMostCommonNumber() {
         //GIVEN
-
+        MostOccurances mostOccurances = new MostOccurances(); //create instance of the class
+        String input = "3,3,4,6"; //create an input
 
         //WHEN
-
+        HashMap <String, Integer> actual = mostOccurances.check(input); //call method and give input, save as Hashmap because method returns Hashmap
 
         //THEN
+        HashMap <String, Integer> expected = new HashMap<>();
+        expected.put("3", 2);
+        expected.put("4", 1);
+        expected.put("6", 1);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
